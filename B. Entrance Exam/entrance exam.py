65c532655d45efd8f9b8e15893e86c6c5106f05d
PeithonKing <amukherjeeniser@gmail.com>
2022-01-29 17:58:27 +0530

    best entrance exam

--- B. Entrance Exam/entrance exam.py
+++ B. Entrance Exam/entrance exam.py
@@ -1,9 +1,6 @@
-d=input
-def i():return map(int,d().split())
-for _ in range(int(d())):
+def i():return map(int,input().split())
+for _ in range(int(input())):
  N,K,E,M=i()
- g = [sum(i()) for _ in range(N)]
+ g=[sum(i()) for _ in range(N)]
  a=sorted(g[:-1])[-K]-g[-1]+1
- if a>M:print("Impossible")
- elif a<0:print(0)
- else:print(a)
+ print("Impossible" if a>M else max(0,a))
