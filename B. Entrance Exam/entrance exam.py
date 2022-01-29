95aeb5cb4f0c7564f9f00d926a87471626d48f89
PeithonKing <amukherjeeniser@gmail.com>
2022-01-29 17:51:09 +0530

    a

--- B. Entrance Exam/entrance exam.py
+++ B. Entrance Exam/entrance exam.py
@@ -1,8 +1,9 @@
-def i():return map(int,input().split())
-def g(a):return [sum(i()) for _ in range(a)]
-for _ in range(int(input())):
-    N,K,E,M=i()
-    a=sorted(g(N-1))[::-1][K-1]-g(1)[0]+1
-    if a>M:print("Impossible")
-    elif a<0:print(0)
-    else:print(a)
+d=input
+def i():return map(int,d().split())
+for _ in range(int(d())):
+ N,K,E,M=i()
+ g = [sum(i()) for _ in range(N)]
+ a=sorted(g[:-1])[-K]-g[-1]+1
+ if a>M:print("Impossible")
+ elif a<0:print(0)
+ else:print(a)
--- count.py
+++ count.py
@@ -1,4 +1,4 @@
-file = r"C:\Users\amukh\Desktop\Code Golf\B. Entrance Exam\a.py"
+file = r"C:\Users\amukh\Desktop\Code Golf\B. Entrance Exam\entrance exam.py"
 
 with open(file) as f:
     a = f.read()
