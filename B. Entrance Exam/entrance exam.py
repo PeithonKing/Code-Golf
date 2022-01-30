dccf571fae57c09f7f3934f82a594c3c246e8721
PeithonKing <amukherjeeniser@gmail.com>
2022-01-31 01:29:52 +0530

    Update entrance exam.py

--- B. Entrance Exam/entrance exam.py
+++ B. Entrance Exam/entrance exam.py
@@ -1,6 +1,6 @@
-def i():return map(int,input().split())
+i=lambda:map(int,input().split())
 for _ in range(int(input())):
  N,K,E,M=i()
- g=[sum(i()) for _ in range(N)]
+ g=[sum(i())for _ in range(N)]
  a=sorted(g[:-1])[-K]-g[-1]+1
- print("Impossible" if a>M else max(0,a))
+ print("Impossible"if a>M else max(0,a))
